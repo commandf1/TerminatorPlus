b9cc8a17af576ad0d6ca0fd0cdf3d4f44f511a63
Gooisnotsocool <164622165+Gooisnotsocool@users.noreply.github.com>
2024-06-23 17:30:45 +0900

    Add AIDataPersistence class (part 1) for saving and loading AI data to and from the file system

--- TerminatorPlus-Plugin/src/main/java/net/nuggetmc/tplus/utils/AIDataPersistence.java
+++ TerminatorPlus-Plugin/src/main/java/net/nuggetmc/tplus/utils/AIDataPersistence.java
@@ -1,20 +1,20 @@
-package net.nuggetmc.tplus.utils;
-import net.nuggetmc.tplus.api.agent.lgagent.ai.neuralNetwork;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.nio.StandardOpenOption;
 
 public class AIDataPersistence {
 
-    private static final String DATA_FILE = "ai_data.dat";
+    private static final String FILE_PATH = "plugins/TerminatorPlus/ai_data.dat";
 
-    public static void saveData(NeuralNetwork network) throws IOException {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(DATA_FILE))) {
-            oos.writeObject(network);
-        }
-    }
+    public static void saveData(Object data) throws IOException {
+        Path path = Paths.get(FILE_PATH);
+        Files.createDirectories(path.getParent);
 
-    public static NeuralNetwork loadData() throws IOException, ClassNotFoundException {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(DATA_FILE)) {
-            return (NeuralNetwork) ois.readObject();
+        try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOputPuts file = Files.newOutputStream(path, standardOpenOption.CREATE+ standardOpenOption.TRUNCATE_EXISTINg));
+            oos.writeObject(data);
         }
     }
 }
