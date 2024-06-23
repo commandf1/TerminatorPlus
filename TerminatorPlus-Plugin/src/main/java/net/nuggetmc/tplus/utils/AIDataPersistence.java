d6eddaf12c4b0467095917ed82276ef91f1b2d47
Gooisnotsocool <164622165+Gooisnotsocool@users.noreply.github.com>
2024-06-23 17:32:49 +0900

    Add AIDataPersistence class for saving AI data to the file system

--- TerminatorPlus-Plugin/src/main/java/net/nuggetmc/tplus/utils/AIDataPersistence.java
+++ TerminatorPlus-Plugin/src/main/java/net/nuggetmc/tplus/utils/AIDataPersistence.java
@@ -3,18 +3,29 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import java.nio.StandardOpenOption;
+import java.nio.StandardOpenOptions;
 
 public class AIDataPersistence {
-
     private static final String FILE_PATH = "plugins/TerminatorPlus/ai_data.dat";
 
     public static void saveData(Object data) throws IOException {
         Path path = Paths.get(FILE_PATH);
         Files.createDirectories(path.getParent);
-
-        try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOputPuts file = Files.newOutputStream(path, standardOpenOption.CREATE+ standardOpenOption.TRUNCATE_EXISTINg));
+        try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(path, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE.EXISTING))) {
             oos.writeObject(data);
+          }
+    }
+
+    public static Object loadData() throws IOException, ClassNotFoundException {
+        Path path = Paths.get(FILE_PATH);
+
+        if ( !files.exists(path) ) {
+            return null;
+        }
+
+        try (ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(path))) {
+            return ois.readObject();
         }
     }
+
 }
