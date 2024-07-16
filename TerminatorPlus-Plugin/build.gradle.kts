493f00ad7cf375cf8955cb56dea084d19383dcd8
commandf1 <mkhjmy@qq.com>
2024-07-16 13:44:48 +0800

    change the java version of the project

--- TerminatorPlus-Plugin/build.gradle.kts
+++ TerminatorPlus-Plugin/build.gradle.kts
@@ -8,8 +8,7 @@ group = "net.nuggetmc"
 description = "TerminatorPlus"
 
 java {
-    // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 repositories {
